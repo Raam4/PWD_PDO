1499f8d7f138c21aed36bb0b0708da1f3d33a2b5
Raam4 <ramiro_cardozo22@hotmail.com>
2021-09-23 17:37:40 -0300

    office

--- Vista/ejercicios/autosPersona.php
+++ Vista/ejercicios/autosPersona.php
@@ -11,14 +11,39 @@ $dataCar = ['dniDuenio'=>$data['nroDni']];
 ?>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-sm-6">
+        <div class="col-sm-8">
             <div class="card border rounded shadow px-2 py-3 mb-4">
-                <p>Autos pertenecientes a:</p>
-                <?php
-                    echo $persona[0]['nroDni'].' | '.$persona[0]['apellido'].' '.$persona[0]['nombre'].' | '.$persona[0]['fechaNac'].' | 
-                    '.$persona[0]['telefono'].' | '.$persona[0]['domicilio'];
-                ?>
-                <table class="table">
+                <table class="table text-center">
+                    <thead>
+                        <tr><th colspan="6" scope="col">Persona</th></tr>
+                    </thead>
+                    <thead>
+                        <tr>
+                            <th scope="col">DNI</th>
+                            <th scope="col">Apellido</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Fecha de Nac.</th>
+                            <th scope="col">Domicilio</th>
+                            <th scope="col">Telefono</th>
+                        </tr>
+                    </thead> 
+                    <tbody>
+                        <?php
+                            echo '<tr>
+                                <th scope="row">'.$persona[0]['nroDni'].'</th>
+                                <td>'.$persona[0]['apellido'].'</td>
+                                <td>'.$persona[0]['nombre'].'</td>
+                                <td>'.$persona[0]['fechaNac'].'</td>
+                                <td>'.$persona[0]['telefono'].'</td>
+                                <td>'.$persona[0]['domicilio'].'</td>
+                            </tr>';
+                        ?>
+                    </tbody>
+                </table>
+                <table class="table text-center">
+                    <thead>
+                        <tr><th colspan="4" scope="col">Autos que le pertenecen</th></tr>
+                    </thead>
                     <thead>
                         <tr>
                             <th scope="col">Patente</th>
